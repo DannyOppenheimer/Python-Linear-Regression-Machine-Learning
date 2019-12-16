3bf5d1b02555ce70a6f4c403c5bac4253114a100
BuildTools <78happydragon78@gmail.com>
2019-12-15 23:19:07 -0500

    Should be final

--- machineLearningLR.py
+++ machineLearningLR.py
@@ -1,4 +1,4 @@
-# Python Linear Regression Machine Learning using Tensor Libraries
+# Python Linear Regression Machine Learning using Tensorflow Libraries
 # ---Credit---
 # Paulo Cortez, University of Minho, GuimarÃ£es, Portugal, http://www3.dsi.uminho.pt/pcortez
 # UCI Machine Learning Repository [https://archive.ics.uci.edu/ml/datasets/student+performance]
@@ -12,25 +12,25 @@ from matplotlib.pyplot import style
 import pickle
 
 # initial options for user
-trainAI = False
-wantTrain = input("Would you like to train the AI? [y/n]: ")
-if wantTrain.lower() == "y" or wantTrain.lower() == "yes" or wantTrain.lower() == "yeah":
-    trainAI = True
-    timesTrain = input("How many training rounds? (don't break computer pl0x)? [num]: ")
+train_AI = False
+want_train = input("Would you like to train the AI? [y/n]: ")
+if want_train.lower() == "y" or want_train.lower() == "yes" or want_train.lower() == "yeah":
+    train_AI = True
+    times_train = input("How many training rounds? (don't break computer pl0x)? [num]: ")
 else:
-    trainAI = False
+    train_AI = False
 
 # read .csv file
-studentData = pd.read_csv("student-dat.csv", sep=";")
-studentData = studentData[["age", "Medu", "Fedu", "traveltime", "studytime", "failures", "famrelquality", "freetime", "goout", "dayalcohol", "weekendalcohol", "health", "absences", "G1", "G2", "G3"]]
+student_data = pd.read_csv("student-dat.csv", sep=";")
+student_data = student_data[["age", "Medu", "Fedu", "traveltime", "studytime", "failures", "famrelquality", "freetime", "goout", "dayalcohol", "weekendalcohol", "health", "absences", "G1", "G2", "G3"]]
 
 # we want to predict the end grade, so the prediction goal is G3
 goalPrediction = "G3"
 
 # X is the set without the final grade (G3) to find all the values we want to train off of
 # Y is the final grad (G3) that we want to predict
-X = np.array(studentData.drop(goalPrediction, 1))
-Y = np.array(studentData[goalPrediction])
+X = np.array(student_data.drop(goalPrediction, 1))
+Y = np.array(student_data[goalPrediction])
 
 # x_train and y_train is training data the computer can read and learns off of
 # x_test and y_test is the prediction data the computer is given to calculate new final grade
@@ -45,78 +45,78 @@ winningDatSet = float(listBest[0].translate({ord('b'): None}))
 foundMoreAccDat = False
 
 # training section
-if trainAI:
-    for x in range(int(timesTrain)):
+if train_AI:
+    for x in range(int(times_train)):
         # refresh the test pool with every run through
         x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
 
         # fit a linear regression like to the data collected
-        linearRegress = linear_model.LinearRegression()
-        linearRegress.fit(x_train, y_train)
+        linear_regress = linear_model.LinearRegression()
+        linear_regress.fit(x_train, y_train)
 
         # accuracy of best fit line
-        linearAcc = linearRegress.score(x_test, y_test)
-        print("Training round ", x + 1, " had an accuracy of ", "%.01f" % (linearAcc * 100), "%", sep="")
+        linear_acc = linear_regress.score(x_test, y_test)
+        print("Training round ", x + 1, " had an accuracy of ", "%.01f" % (linear_acc * 100), "%", sep="")
 
         # did any of the training rounds surpass the pickled data file's accuracy?
-        if linearAcc > winningDatSet:
+        if linear_acc > winningDatSet:
             foundMoreAccDat = True
-            winningDatSet = linearAcc
+            winning_dat_set = linear_acc
 
             # save that value to a txt file
             with open("Test Scores/bestLearn.txt", "w") as h:
-                h.write(str(winningDatSet))
-            print("Training round ", x + 1, " has the running best accuracy of ", "%.01f" % (winningDatSet * 100), "%", sep="")
+                h.write(str(winning_dat_set))
+            print("Training round ", x + 1, " has the running best accuracy of ", "%.01f" % (winning_dat_set * 100), "%", sep="")
 
             # create a new pickle file if there isn't one, or write to an existing one
             with open("studentModel.pickle", "wb") as f:
                 # dump linear regression into above pickle file
-                pickle.dump(linearRegress, f)
+                pickle.dump(linear_regress, f)
 
     if foundMoreAccDat:
         print("Final accuracy: ", "%.01f" % (winningDatSet * 100), "%", sep="")
     else:
-        print("This training run didn't get a higher accuracy than a saved one :( current set has an accuracy of: ", "%.01f" % (winningDatSet * 100), "%", sep="")
+        print("This training run didn't get a higher accuracy than a saved one :( current set has an accuracy of: ", "%.01f" % (winning_dat_set * 100), "%", sep="")
 
 
 # open pickle file to access other information
 pickleIn = open("studentModel.pickle", "rb")
-linearRegressData = pickle.load(pickleIn)
+linear_regress_data = pickle.load(pickleIn)
 print("\n***********************************************")
-print("* Coefficients: ", linearRegressData.coef_)
-print("* Intercept: ", linearRegressData.intercept_)
+print("* Coefficients: ", linear_regress_data.coef_)
+print("* Intercept: ", linear_regress_data.intercept_)
 print("***********************************************")
 
 # print out computer's predictions for the x_test data
 print("\nHow to read: <guessed score> [students background] <actual score>")
-predictions = linearRegressData.predict(x_test)
+predictions = linear_regress_data.predict(x_test)
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
 
 
 # graph plotting to see the relationships for individual pieces of data
-def ask():
-    graphReq = input("\nWhich graph would you like to display?")
+def ask_graph():
+    graph_req = input("\nWhich graph would you like to display?")
     try:
         style.use("ggplot")
-        pyplot.scatter(studentData[graphReq], studentData["G3"])
-        pyplot.xlabel(graphReq)
+        pyplot.scatter(student_data[graph_req], student_data["G3"])
+        pyplot.xlabel(graph_req)
         pyplot.ylabel("Final Grade")
         pyplot.show()
     except KeyError:
         print("Oops! That's not an option. These are though!")
         print("age", "Medu", "Fedu", "traveltime", "studytime", "failures", "famrelquality", "freetime", "goout", "dayalcohol", "weekendalcohol", "health", "absences", "G1", "G2", "G3", sep=", ")
-        ask()
+        ask_graph()
 
 
 # prompt graph
-exitRequest = input("Would you like to see a graph? [y/n]: ")
+exit_request = input("Would you like to see a graph? [y/n]: ")
 
-if exitRequest.lower() == "y" or exitRequest.lower() == "yes" or exitRequest.lower() == "yeah":
-    ask()
+if exit_request.lower() == "y" or exit_request.lower() == "yes" or exit_request.lower() == "yeah":
+    ask_graph()
 
 # User indicates whether or not they would like to save the most accurate data set
-overwriteLearn = input("Would you like to overwrite the most accurate data set and set it to 0? [y/n]: ")
-if overwriteLearn.lower() == "y" or overwriteLearn.lower() == "yes" or overwriteLearn.lower() == "yeah":
+overwrite_learn = input("Would you like to overwrite the most accurate data set and set it to 0? [y/n]: ")
+if overwrite_learn.lower() == "y" or overwrite_learn.lower() == "yes" or overwrite_learn.lower() == "yeah":
     with open("Test Scores/bestLearn.txt", "w") as h:
         h.write(str("0.0"))
